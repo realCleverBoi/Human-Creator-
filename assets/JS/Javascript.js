52d7845c6008b4a4b1187abc82482c7f814e3401
realCleverBoi <akinbamipopoola@gmail.com>
2019-07-31 09:14:20 +0100

    Javascript code updated with conditions to check if Persons name is specified or not
    images moved to assets/img folder

--- assets/JS/Javascript.js
+++ assets/JS/Javascript.js
@@ -3,7 +3,9 @@ function Selector(gender) {
 	gen = gender;
 	created.innerHTML = "";
 }
+
 function Humancreator() {
+	
 	class Human{
 		constructor(){
 			this.name = oruko.value;
@@ -22,33 +24,45 @@ function Humancreator() {
 			+"<b>Ears:</b> " +this.ear + "<br>"+"<br>"
 			+"<b>Legs:</b> " +this.leg + "<br>"+"<br>";
 		}
+		displayer(){
+			created.innerHTML = "<u style='color:red;font-weight:bolder;font-size:25px;position:relative;top:150px;'>Notice:</u> "
+		}
 	}
 	
 	class Male extends Human{
 		bear = "Yes";
 		hair = "Short";
-		avatar = "<img src='m.png' style='width:50px;height:50px;position:relative;bottom:320px;left:120px;border-radius:25px;'>";
+		avatar = "<img src='assets/img/m.png' style='width:50px;height:50px;position:relative;bottom:320px;left:120px;border-radius:25px;'>";
 	}
 
 	class Female extends Human{
 		bear = "No";
 		hair = "Long";
-		avatar = "<img src='f.png' style='width:50px;height:50px;position:relative;bottom:320px;left:120px;border-radius:25px;border:0.1px solid black;'>";
+		avatar = "<img src='assets/img/f.png' style='width:50px;height:50px;position:relative;bottom:320px;left:120px;border-radius:25px;border:0.1px solid black;'>";
 	}
-
-	if (gen == "m") {
-		let pman = new Male;
-		created.innerHTML = "<b>You created a man</b>" + "<br>"+"<br>"
-		+pman.activator() 
-		+"<b>Bear:</b> " +pman.bear + "<br>"+"<br>"
-		+"<b>Hair:</b> " +pman.hair  + "<br>"+"<br>"
-		+pman.avatar;
-	} else if (gen == "f") {
-		let pman = new Female;
-		created.innerHTML = "<b>You created a woman</b>" + "<br>"+"<br>"
-		+pman.activator() 
-		+"<b>Bear:</b> " +pman.bear + "<br>"+"<br>"
-		+"<b>Hair:</b> " +pman.hair + "<br>"+"<br>"
-		+pman.avatar;;
+	class Nothing extends Human{
+		info = "<b style='font-weight:bolder;font-size:18px;position:relative;top:150px;'> Person's name not specified!</b>"
+	}
+	if (oruko.value != "") {
+		if (gen == "m") {
+			let pman = new Male;
+			created.innerHTML = "<b>You created a man</b>" + "<br>"+"<br>"
+			+pman.activator() 
+			+"<b>Bear:</b> " +pman.bear + "<br>"+"<br>"
+			+"<b>Hair:</b> " +pman.hair  + "<br>"+"<br>"
+			+pman.avatar;
+		} else if (gen == "f") {
+			let pman = new Female;
+			created.innerHTML = "<b>You created a woman</b>" + "<br>"+"<br>"
+			+pman.activator() 
+			+"<b>Bear:</b> " +pman.bear + "<br>"+"<br>"
+			+"<b>Hair:</b> " +pman.hair + "<br>"+"<br>"
+			+pman.avatar;;
+		}
+	} else {
+		// alert("Please Enter Person's name(s)");
+		let n = new Nothing;
+		n.displayer();
+		created.innerHTML += n.info;
 	}
 }	
